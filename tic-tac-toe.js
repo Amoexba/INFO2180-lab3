ef270cfb52e391717d14e36366c269d7a58ccd73
Amoexba <emeika.osbourne8@gmail.com>
2025-10-16 03:44:08 -0500

    Added JavaScript to style the game board squares on load

---
+++ tic-tac-toe.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function() {
+    
+    const board = document.getElementById('board');
+
+    const squares = board.querySelectorAll('div');
+    
+    squares.forEach(function(square) {
+        square.classList.add('square');
+    });
+});
